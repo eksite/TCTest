26ac4b030b7316d3c4d4fe1f0d09403b4c4c1604
eksite <denisandipod@gmail.com>
2020-04-05 13:22:22 +0300

    bug fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,6 @@
-// eslint-disable-next-line no-unused-vars
 import React, {useState, useEffect} from 'react';
 import useLoadData from './hooks/useLoadData';
 import moment from 'moment';
-
-
-const App=()=>{
-    const [imagineTime, setImagineTime]=useState([]);
-    const data = useLoadData( 'https://raw.githubusercontent.com/denissokolov/tc-internship-task/master/launches.json' );
-    const extendedData = data.map(extendData);
-    useEffect(() => {
-        const timer=setTimeout(() => {
-            const newState=extendedData.map(obj => {
-                if (obj.notInf) {
-                    return { ...obj, countdown: obj.notInf, status: 'TBD' };
-                }
-                if (obj.extraEventTimeStamp) {
-                    return { ...obj, status:defineStatus(obj), countdown: timeFormatter(obj.extraEventTimeStamp - moment().unix())  };
-                }
-                return { ...obj,status:defineStatus(obj), countdown: timeFormatter(obj.eventTimeStamp - moment().unix())};
-            });
-            setImagineTime(newState);
-        },1000);
-        return () => clearTimeout(timer);
-    }, [extendedData]);
-    return imagineTime;
-};
 
 const baseDateFormatter = ( year, month, day, hour, min ) => {
     const launchTime = moment({ y: year, M: month, d: day, h: hour, m: min }).format('D.MM.YYYY Qo , h:mm a');
@@ -32,26 +8,12 @@ const baseDateFormatter = ( year, month, day, hour, min ) => {
     return [launchTime, timeStamp];
 };
 
-const timeFormatter = (timeStamp) => {
-    if (timeStamp<0){
-        timeStamp=timeStamp*(-1);
-    }
-    return `${Math.floor(timeStamp/(60*60*24))}d ${Math.floor(timeStamp/(60*60))%24}h ${Math.floor(timeStamp/(60))%60}m`;
-};
-
-const defineStatus = (obj) => {
-    if (obj.extraEventTimeStamp){
-        return obj.extraEventTimeStamp - moment().unix()<0 ? 'Soon' : 'Planned';
-    }
-    return obj.eventTimeStamp - moment().unix()<0 ? 'Launched' : 'Planned';
-};
-
 const extendData = obj => {
     let timeStamp;
     let launchTime; 
     const { launch: { years, months, date, hours, minutes, quarter } } = obj;
     if (!quarter) {
-        return {...obj, notInf: 'TBD', launch: 'In our plan' };
+        return {...obj, noInf: 'TBD', launch: 'In our plan' };
     }
     if (!months)  {
         switch (quarter) {
@@ -76,6 +38,42 @@ const extendData = obj => {
     }
     [launchTime, timeStamp] = baseDateFormatter( years, months-1, date, hours, minutes );
     return {...obj, eventTimeStamp: timeStamp, launch: launchTime};
+};
+
+const timeFormatter = (timeStamp) => {
+    if (timeStamp<0){
+        timeStamp = timeStamp*(-1);
+    }
+    return `${Math.floor(timeStamp/(60*60*24))}d ${Math.floor(timeStamp/(60*60))%24}h ${Math.floor(timeStamp/(60))%60}m`;
+};
+
+const defineStatus = (obj) => {
+    if (obj.extraEventTimeStamp){
+        return obj.extraEventTimeStamp - moment().unix()<0 ? 'Soon' : 'Planned';
+    }
+    return obj.eventTimeStamp - moment().unix()<0 ? 'Launched' : 'Planned';
+};
+
+const App=()=>{
+    const [displayedTime, setDisplayedTime] = useState([]);
+    const data = useLoadData('https://raw.githubusercontent.com/denissokolov/tc-internship-task/master/launches.json');
+    const extendedData = data.map(extendData);
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            const newState = extendedData.map(obj => {
+                if (obj.noInf) {
+                    return { ...obj, countdown: obj.noInf, status: 'TBD' };
+                }
+                if (obj.extraEventTimeStamp) {
+                    return { ...obj, status:defineStatus(obj), countdown: timeFormatter(obj.extraEventTimeStamp - moment().unix())  };
+                }
+                return { ...obj,status:defineStatus(obj), countdown: timeFormatter(obj.eventTimeStamp - moment().unix())};
+            });
+            setDisplayedTime(newState);
+        }, 1000);
+        return () => clearTimeout(timer);
+    }, [extendedData]);
+    return displayedTime;
 };
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import Styled from 'styled-components';
 
-const StyledHeader=Styled.header`
-    display:flex;
-    width:100%;
-    height:74px;
+const StyledHeader = Styled.header`
+    display: flex;
+    width: 100%;
+    height: 74px;
     background-color: #121839;
 `;
 
-const StyledParagraph=Styled.p`
+const StyledParagraph = Styled.p`
     width: 262px;
     height: 26px;
     font-size: 22px;
     line-height: 26px;
     color: #FFFFFF;
-    margin-left:108px;
-    margin-top:32px;
+    margin-left: 108px;
+    margin-top: 32px;
 `;
 
 const Header = () => {
@@ -23,7 +23,6 @@ const Header = () => {
         <StyledHeader>
             <StyledParagraph>SPACE-X LAUNCHES 2020</StyledParagraph>
         </StyledHeader>
-
     );
 };
 
